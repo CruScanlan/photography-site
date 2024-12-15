dd91baf93472d37270109c81ef845ae8b1c3555b
Cru Scanlan <cruscanlan@gmail.com>
2024-12-15 13:02:35 +1000

    Add missing image source

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ module.exports = withPlaiceholder({
             {
                 protocol: 'https',
                 hostname: 'images.ctfassets.net'
+            },
+            {
+                protocol: 'https',
+                hostname: 'downloads.ctfassets.net'
             }
         ],
         deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
