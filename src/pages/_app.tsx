ed30c4880303ae8a710fca2f06babdff097cbe92
Cru Scanlan <cruscanlan@gmail.com>
2024-12-15 12:30:27 +1000

    Fix Build Error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
-import store from 'store/store';
+import store, { persistor } from 'store/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { CartProvider } from 'use-shopping-cart';
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -36,32 +36,34 @@ library.add(
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return ( //@ts-ignore
         <ReduxProvider store={store}>
-            <CartProvider
-                cartMode="checkout-session"
-                stripe={process.env.NEXT_PUBLIC_STRIPE_KEY_PUBLISHABLE || ''}
-                currency="AUD"
-            >
-                <Script 
-                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                    strategy="afterInteractive"
-                />
-                <Script
-                    id="google-analytics"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){window.dataLayer.push(arguments);}
-                            gtag('js', new Date());
+            <PersistGate loading={null} persistor={persistor}>
+                <CartProvider
+                    cartMode="checkout-session"
+                    stripe={process.env.NEXT_PUBLIC_STRIPE_KEY_PUBLISHABLE || ''}
+                    currency="AUD"
+                >
+                    <Script 
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                        strategy="afterInteractive"
+                    />
+                    <Script
+                        id="google-analytics"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){window.dataLayer.push(arguments);}
+                                gtag('js', new Date());
 
-                            gtag('config', '${GA_TRACKING_ID}', { 
-                                page_path: window.location.pathname,
-                            });
-                        `,
-                    }}
-                />
-                <Component {...pageProps} />
-            </CartProvider>
+                                gtag('config', '${GA_TRACKING_ID}', { 
+                                    page_path: window.location.pathname,
+                                });
+                            `,
+                        }}
+                    />
+                    <Component {...pageProps} />
+                </CartProvider>
+            </PersistGate>
         </ReduxProvider>
     );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,21 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from './customStorage'
 
 import cartReducer from './cartReducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 
 export function makeStore() {
     return configureStore({
         reducer: { 
-            cart: cartReducer
+            cart: persistedReducer
         },
         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
             serializableCheck: {
@@ -17,11 +26,10 @@ export function makeStore() {
 };
 
 const store = makeStore();
+export const persistor = persistStore(store)
 
 export type AppState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
-
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   AppState,
