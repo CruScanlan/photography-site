9ec6659c03c64b1573bd4ecb9b2968eb36d0fae7
Cru Scanlan <cruscanlan@gmail.com>
2024-12-15 15:23:49 +1000

    Update Sitemap

--- src/pages/server-sitemap-index.xml/index.tsx
+++ src/pages/server-sitemap-index.xml/index.tsx
@@ -6,7 +6,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     let urls = [];
     //Get Image URLS
     const landscapeImages = await contentful.getEntries<any>({include: 1, content_type: 'landscapeImage'});
-    urls.push(...landscapeImages.items.map(item => `/image/${item.fields.slug}`));
+    urls.push(...landscapeImages.items.map(item => `https://cruscanlan.com/image/${item.fields.slug}`));
+
+    //Get gallery page URLS
+    const galleryPage = await contentful.getEntry<any>('5MUgow4FEnQHKNRQI5p7Cr', {include: 2});
+    urls.push(...galleryPage.fields.photoCollections.map(item => `https://cruscanlan.com/gallery/${item.fields.slug}`));
 
     return getServerSideSitemapIndexLegacy(ctx, urls);
 }
