a3bdf6e643ed7371798cde500c482250275b5c53
Cru Scanlan <cruscanlan@gmail.com>
2025-10-12 23:20:12 +1000

    Next 16

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        turbopackFileSystemCacheForDev: true,
+    },
     images: {
         formats: ['image/avif', 'image/webp'],
         remotePatterns: [
@@ -13,7 +16,7 @@ const nextConfig = {
             }
         ],
         deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        imageSizes: [32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 31536000, // Cache images for 1 year (seconds)
         dangerouslyAllowSVG: false,
         contentDispositionType: 'inline',
