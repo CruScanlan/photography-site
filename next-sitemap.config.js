29822e53578cae3b595ff6514cefa6934d454075
Cru Scanlan <cruscanlan@gmail.com>
2025-10-12 22:38:49 +1000

    Exclude store

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,10 +3,22 @@ module.exports = {
     siteUrl: process.env.SITE_URL || 'https://cruscanlan.com',
     generateRobotsTxt: true, // (optional)
     sitemapSize: 7000,
-    exclude: ['/server-sitemap.xml'], // <= exclude here
+    // Exclude server-generated sitemap and all store pages (store is disabled)
+    exclude: [
+        '/server-sitemap.xml',
+        '/store',
+        '/store/*',
+    ],
     robotsTxtOptions: {
         additionalSitemaps: [
             'https://cruscanlan.com/server-sitemap-index.xml', // <==== Add here
+        ],
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: ['/store'], // Disallow store pages in robots.txt
+            },
         ],
     },
 }
